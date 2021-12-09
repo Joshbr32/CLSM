74fc47c7c595ee4e742a0d4451fc711e33a5b145
Kiishi201 <68674972+Kiishi201@users.noreply.github.com>
2021-12-09 14:55:07 -0500

    toolbar

--- app/src/main/java/com/example/clsm/MainActivity.java
+++ app/src/main/java/com/example/clsm/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.clsm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.gesture.GestureLibrary;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    GestureLibrary lib;
+
     protected static final String ACTIVITY_NAME = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+        //(myToolbar);
+        //Toolbar toolbar =  findViewById(R.id.toolbar2);
+        //setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+
     }
+
+
 
 
     public void timesheet(View view) {
@@ -60,4 +75,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent= new Intent(this, TimeSheetActivity.class);
         startActivity(intent);
     }
+
+    public class ViewOverlay
+            extends Object {};
+
+
+
+
+
+
 }
