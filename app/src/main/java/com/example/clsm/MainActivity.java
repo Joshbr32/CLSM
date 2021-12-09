ae1a74c00fec89828decc8dc17c5f54311263168
Kevin Xie <66642267+kevinprimex@users.noreply.github.com>
2021-12-08 22:28:14 -0500

    activity_work_order.xml

--- app/src/main/java/com/example/clsm/MainActivity.java
+++ app/src/main/java/com/example/clsm/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.clsm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button ts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ts = findViewById(R.id.timesheet_button);
+        ts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TimeSheetActivity.class);
+                startActivity(intent );
+
+
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/clsm/TimeSheetActivity.java
+++ app/src/main/java/com/example/clsm/TimeSheetActivity.java
@@ -2,13 +2,70 @@ package com.example.clsm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TimeSheetActivity extends AppCompatActivity {
+
+    EditText name,timeIn,timeOut,date,sv;
+    Button save,page;
+    SharedPreferences sharedPreferences;
+    String n1,t1,t2,d1,s1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_sheet);
+
+        name = findViewById(R.id.TimeName1);
+        sv = findViewById(R.id.timeSupervisor1);
+        timeIn = findViewById(R.id.TimeIn1);
+        timeOut = findViewById(R.id.TimeOut1);
+        date = findViewById(R.id.TimeDate1);
+        save = findViewById(R.id.time_save);
+        page = findViewById(R.id.time_clear);
+
+        sharedPreferences = getSharedPreferences("preference", Context.MODE_PRIVATE);
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = name.getText().toString();
+                t1 = timeIn.getText().toString();
+                t2 = timeOut.getText().toString();
+                d1 = date.getText().toString();
+                s1 = sv.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("n1", n1);
+                editor.putString("t1",t1);
+                editor.putString("t2", t2);
+                editor.putString("d1",d1);
+                editor.putString("s1", s1);
+                editor.commit();
+
+                Toast.makeText(TimeSheetActivity.this, "Time Successfully Logged!", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+        page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TimeSheetActivity.this, TimePage.class);
+                startActivity(intent );
+
+
+
+            }
+        });
     }
 }
