546de62affd487605b0131debaad295d931e4c2d
Kiishi201 <68674972+Kiishi201@users.noreply.github.com>
2021-12-09 21:14:49 -0500

    Toolbar

--- app/src/main/java/com/example/clsm/MainActivity.java
+++ app/src/main/java/com/example/clsm/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.clsm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.gesture.GestureLibrary;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,15 +24,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomTool = findViewById(R.id.bottom_toolbar);
+        bottomTool.setOnNavigationItemSelectedListener(toolListener);
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener toolListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.stopwatch:
+                    selectedFragment = new StopwatchFragment();
+                    break;
+                case R.id.filing:
+                    selectedFragment = new FilingCabinetFragment();
+                    break;
+                case R.id.clipboard:
+                    selectedFragment = new ClipboardFragment();
+                    break;
+                case R.id.questions:
+                    selectedFragment = new QuestionFragment();
+                    break;
+
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+            return true;
+        }
+    };
         //Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
         //(myToolbar);
         //Toolbar toolbar =  findViewById(R.id.toolbar2);
         //setSupportActionBar(toolbar);
-        Toolbar toolbar = findViewById(R.id.toolbar2);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar2);
+        //setSupportActionBar(toolbar);
 
 
-    }
+
 
 
 
@@ -76,8 +116,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public class ViewOverlay
-            extends Object {};
 
 
 
